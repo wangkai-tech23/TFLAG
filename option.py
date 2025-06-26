4dda2e4dba228500220a903cd766fb07c6424f62
jiangdie666 <72592733+jiangdie666@users.noreply.github.com>
2025-06-26 10:14:25 +0800

    Update option.py

--- option.py
+++ option.py
@@ -1,20 +1,21 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Denoise')
+parser = argparse.ArgumentParser(description='')
 parser.add_argument('--dir_data', type=str, default='./dataset')
-parser.add_argument('--data_set', type=str, default='cadets', choices=('theia', 'clearscope','cadets'))
+parser.add_argument('--data_set', type=str, default='cadets', choices=('cadets'))
+parser.add_argument('--module_type', type=str, default='graph_attention', choices=('graph_attention'))
 
+# add
+parser.add_argument('--anomaly_alpha', type=float, default=0.5, help="gnn anomaly loss param")
+parser.add_argument('--supc_alpha', type=float, default=1e-1, help="gnn supc loss param")
+parser.add_argument('--memory_size', type=int, default=20000, help="gdn memory_size")
+parser.add_argument('--sample_size', type=int, default=8000, help="gdn sample_size")
 
-parser.add_argument('--anomaly_alpha', type=float, default=5e-3, help="gnn anomaly loss param")
-parser.add_argument('--memory_size', type=int, default=5000, help="gdn memory_size")
-parser.add_argument('--sample_size', type=int, default=2000, help="gdn sample_size")
-parser.add_argument('--mask_label', action='store_true', default=False)
-parser.add_argument('--mask_ratio', type=float, default=0.5)
 ##data param
 parser.add_argument('--n_neighbors', type=int, default=20, help='Maximum number of connected edge per node')
 parser.add_argument('--batch_size', type=int, default=256)
-parser.add_argument('--n_epochs', type=int, default=5)
-parser.add_argument('--num_data_workers', type=int, default=8)
+parser.add_argument('--n_epochs', type=int, default=3)
+parser.add_argument('--num_data_workers', type=int, default=24)
 parser.add_argument('--gpus', type=int, default=1)
 parser.add_argument('--accelerator', type=str, default='ddp')
 
